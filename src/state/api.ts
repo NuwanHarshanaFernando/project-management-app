9e58d621d984fc183ae3a5f159973b9e5664d3fe
NuwanHarshanaFernando <nuwanhfernando@gmail.com>
2026-06-01 20:11:43 +0530

    Created endpoints getProjects and createProject

--- src/state/api.ts
+++ src/state/api.ts
@@ -66,8 +66,21 @@ export const api = createApi({
         baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL
     }),
     reducerPath: "api",
-    tagTypes: [],
-    endpoints: (builder) => ({})
+    tagTypes: ["Projects"],
+    endpoints: (build) => ({
+        getProjects: build.query<Project[], void>({
+            query: () => "projects",
+            providesTags: ["Projects"],
+        }),
+        createProject: build.mutation<Project, Partial<Project>>({
+            query: (project) => ({
+                url: "projects",
+                method: "POST",
+                body: project,
+            }),
+            invalidatesTags: ["Projects"],
+        }),
+    })
 });
 
-export const {} = api;
+export const { useGetProjectsQuery, useCreateProjectMutation } = api;
